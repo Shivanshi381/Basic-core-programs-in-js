5f7549218cf5863f5502ae1f35a9a9cf920edc09
Shivanshi Tiwari <shivanshitiwari381@gmail.com>
2024-02-05 06:33:46 +0530

    Update script.js

--- 6/script.js
+++ 6/script.js
@@ -7,22 +7,19 @@ const rl = readline.createInterface({
 function computePrimeFactors(N) {
     console.log(`Prime factors of ${N} are:`);
     
-    // Print the number of 2s that divide N
     while (N % 2 === 0) {
         console.log(2);
         N = N / 2;
     }
 
-    // N must be odd at this point, so a skip of 2 ( i = i + 2) can be used
     for (let i = 3; i * i <= N; i = i + 2) {
-        // While i divides N, print i and divide N
         while (N % i === 0) {
             console.log(i);
             N = N / i;
         }
     }
 
-    // If N is a prime number greater than 2
+  
     if (N > 2) {
         console.log(N);
     }
