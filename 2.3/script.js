5fa79036f4de844d943fef7417fcaff2c1bd985a
Shivanshi Tiwari <shivanshitiwari381@gmail.com>
2024-02-05 08:32:53 +0530

    Update script.js

--- 2.3/script.js
+++ 2.3/script.js
@@ -4,16 +4,16 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Enter the value of x: ", function (xInput) {
-    rl.question("Enter the value of y: ", function (yInput) {
-        const x = parseFloat(xInput);
-        const y = parseFloat(yInput);
+rl.question("Enter the value of x: ", function (xvalInput) {
+    rl.question("Enter the value of y: ", function (yvalInput) {
+        const x = parseFloat(xvalInput);
+        const y = parseFloat(yvalInput);
 
         if (!isNaN(x) && !isNaN(y)) {
             const distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
             console.log(`Euclidean distance from point (${x}, ${y}) to the origin (0, 0): ${distance}`);
         } else {
-            console.log("Invalid input. Please enter valid numerical values for x and y.");
+            console.log("Invalid input.");
         }
 
         rl.close();
